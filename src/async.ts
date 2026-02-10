f7dd7bd337850b0ca74b3d06a6f355199ff06256
Susan Cheng <susan.doggie@gmail.com>
2026-02-11 00:17:27 +0800

    clean

--- src/async.ts
+++ src/async.ts
@@ -2,7 +2,7 @@
 //  async.js
 //
 //  The MIT License
-//  Copyright (c) 2015 - 2025 Susan Cheng. All rights reserved.
+//  Copyright (c) 2015 - 2026 Susan Cheng. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
